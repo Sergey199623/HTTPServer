6874a8a9ae9072cf3261529b133d42be9193b3d1
Sergey <Belyakovsergeyrus@yandex.ru>
2020-04-10 00:29:43 +0300

    Add files via upload

---
+++ HttpServer/src/ru/serverhttp/Main.java
@@ -0,0 +1,47 @@
+package ru.serverhttp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(8080)) {
+            while (true) {
+                Socket socket = serverSocket.accept();
+
+                System.out.println("New client connected");
+
+                try (BufferedReader input = new BufferedReader(
+                        new InputStreamReader(
+                                socket.getInputStream(), UTF_8));
+                     PrintWriter output = new PrintWriter(socket.getOutputStream())) {
+                    while (!input.ready()) ;
+
+                    new Thread(() -> {
+
+                    }).start();
+
+                    System.out.println();
+                    while (input.ready()) {
+                        System.out.println(input.readLine());
+                    }
+
+                    output.println("HTTP/1.1 200 OK");
+                    output.println("Content-Type: text/html; charset=utf-8");
+                    output.println();
+                    output.println("<h1>Приветствую!</h1>");
+                    output.flush();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
